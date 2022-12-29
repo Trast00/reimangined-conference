c9f1da8c31dae6867ac34ea61054e63f27229e08
Allassane <dickoallassane1997@gmail.com>
2022-12-29 21:02:48 +0100

    Update schedule.html : Finish Table layout positioning (mobile, desktop version)

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ btnCloseMenu.addEventListener('click', () => {
 });
 
 /* Load Speaker Dynamically */
-function loadSpeakers(listSpeaker) {
+const loadSpeakers = (listSpeaker) => {
   const ulListSpeaker = document.getElementById('list-speakers');
   ulListSpeaker.innerHTML = ''
   listSpeaker.forEach((speaker) => {
@@ -70,7 +70,7 @@ const listSpeakers = [
     name: 'James Clear', subName: 'Author of best sellers, Speaker, Weightlifter.', biographie: 'The author of the #1 New York Times bestseller, Atomic Habits, which has sold more than 9 million copies worldwide and has been translated into more than 50 languages.', image: 'images/James Clear.jpg',
   },
 ];
-loadSpeakers(listSpeakers.slice(0, 2));
+/*loadSpeakers(listSpeakers.slice(0, 2));
 
 btnShowMore.addEventListener('click', () => {
   const isShown = (btnShowMore.innerHTML === "Show More")
@@ -82,7 +82,129 @@ btnShowMore.addEventListener('click', () => {
     loadSpeakers(listSpeakers.slice(0, 2));
   }
 })
+*/
+/* Load List event dynamically */
 
+const loadEvents = (listEvents) => {
+  console.log('Event')
+  const calendar = document.querySelector('.calendar')
+  listEvents.forEach((row) => {
+    const liRow = document.createElement('li')
+    liRow.classList.add('row', 'border-bottom')
+    
+    const pTime = document.createElement('p')
+    pTime.textContent = row.time
+    pTime.classList.add('date')
+
+    const ulListEvent = document.createElement('ul')
+    ulListEvent.classList.add('list-event')
+    
+    row.events.forEach((event) => {
+      const liEvent = document.createElement('li')
+      if (event.description === undefined){
+        liEvent.classList.add('event-simple')
+        liEvent.textContent = event.title
+        ulListEvent.append(liEvent)
+        return
+      }else {
+
+      }
+      liEvent.classList.add('event')
+
+      const detail = document.createElement('div')
+      detail.classList.add('event-detail')
+
+      const title = document.createElement('div')
+      title.classList.add('event-title', 'row')
+
+      const icons = document.createElement('i')
+      icons.classList.add('fa-solid', 'fa-mug-hot')
+      
+      const hTitle = document.createElement('h6')
+      hTitle.classList.add('event-title')
+      hTitle.textContent = event.title
+
+      const description = document.createElement('p')
+      description.classList.add('event-description')
+      description.innerHTML = event.description
+      
+
+      const subDetail = document.createElement('div')
+      subDetail.classList.add('event-subdetail')
+
+      const localisation = document.createElement('p')
+      localisation.classList.add('event-localisation')
+      localisation.textContent = event.location
+      const moreInfo = document.createElement('p')
+      moreInfo.classList.add('event-question')
+      moreInfo.textContent = event.moreInfo
+
+      if (row.events.length === 1){
+        moreInfo.style.display = "none"
+      }
+      else {
+        title.style.marginBottom = "10px"
+        description.style.height = "150px"
+        subDetail.style.width = "100%"
+      }
+
+      /* Appends */
+      title.append(icons, hTitle)
+      detail.append(title, description)
+
+      subDetail.append(localisation, moreInfo)
+
+      liEvent.append(detail, subDetail)
+
+      ulListEvent.append(liEvent)
+    })
+
+    /* Appends */
+    liRow.append(pTime, ulListEvent)
+
+    calendar.append(liRow)
+  })
+}
+
+const listEvents = [
+  {time: "09:30 - 10:00", events: [{title: "Registration"}]},
+  {time: "10:00 - 10:10", 
+    events: [
+      {title: "Opening Remarks"},
+      {title: "TBD"}
+    ]
+  },
+  {time: "10:00 - 10:40('30')", 
+    events: [
+     {title: "[Keynote] Humanology", type: "lecture", description:"Prof. Kinam Cheon", location: "Main Auditoruim"}
+    ]
+  },
+  {time: "10:00 - 10:10", 
+    events: [
+      {title: "Open Business", type: "lecture", description:"What point should be ensured in order to success in open business ?", location: "Main Auditoruim", moreInfo: "What's Open Business"},
+      {title: "Sharing City", type: "lecture", description:"What's point should be ensured in order to success in sharing city ?", location: "location", moreInfo: "What's a sharing city"},
+      {title: "Sharing City", type: "lecture", description:"What's point should be ensured in order to success in sharing city ?", location: "location", moreInfo: "What's a sharing city"}
+    ]
+  },
+  {time: "10:00 - 10:40('30')", 
+    events: [
+      {title: "[Keynote] Humanology", type: "lecture", description:"Prof. Kinam Cheon", location: "Main Auditoruim"}
+      ]
+    },
+  {time: "10:00 - 10:10", 
+    events: [
+      {title: "Open Business", type: "lecture", description:"What point should be ensured in order to success in open business ?", location: "Main Auditoruim", moreInfo: "What's Open Business"},
+      {title: "Sharing City", type: "lecture", description:"What's point should be ensured in order to success in sharing city ?", location: "location", moreInfo: "What's a sharing city"},
+    ]
+  },
+  {time: "10:00 - 10:40('30')", 
+    events: [
+      {title: "[Keynote] Humanology", type: "lecture", description:"Prof. Kinam Cheon", location: "Main Auditoruim"}
+    ]
+  }
+]
+
+loadEvents(listEvents)
 /* To do:
  -All speaker must be display without show more btn when screen size change
 */
